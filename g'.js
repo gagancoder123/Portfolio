e7b47335dede76395b14b3423c4afb36374ac42c
gagancoder123 <gagankumarnetid@gmail.com>
2023-11-06 23:42:18 +0530

    initial commit

---
+++ g'.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("myButton");
+
+    // Add a click event listener to the button
+    button.addEventListener("click", function () {
+        alert("Button clicked!");
+    });
+});
